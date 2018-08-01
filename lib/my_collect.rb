1a167119b7398527a6d4e4ff162b26b49f203049
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-01 16:49:15 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,10 +3,10 @@ def my_collect (array)
   collection = []
 
   while i < array.length
-    collection << yield (array[i])
+    collection << yield(array[i])
     i += 1
   end
 
   collection
-  
+
 end
